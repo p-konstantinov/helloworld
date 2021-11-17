e7bc9ae2bf59b884ab8a7722bd53ff188e817436
PavelKonstantinov <aurelionsoul@icloud.com>
2021-11-17 20:17:28 +0300

    Initial Commit

---
+++ Shared/helloworldApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloworldApp.swift
+//  Shared
+//
+//  Created by Павел Константинов on 17.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct helloworldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
